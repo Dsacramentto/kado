704477415aa72f1ed62eda7cf91d93a36ba93596
Daniel Sacramento <98630611+Dsacramentto@users.noreply.github.com>
2024-06-02 12:29:10 -0300

    Adicionando menção ao criador

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 onload = () =>{
         document.body.classList.remove("container");
 };
+
+
+//Importante falar que o código não é meu, apenas personalizei!
+
+//Segue o github onde achei o código: https://github.com/septiandwica/kado
